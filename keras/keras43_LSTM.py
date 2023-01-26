59ddcc615194afea4fda27c492e498b5053f70d9
MYeonP <pmyeon608@gmail.com>
2023-01-26 19:38:13 +0900

    Create keras43_LSTM.py

---
+++ keras/keras43_LSTM.py
@@ -0,0 +1,41 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, SimpleRNN, LSTM
+
+#1. 데이터
+dataset = np.array([1,2,3,4,5,6,7,8,9,10])  # (10, )
+# y = ???
+
+x = np.array([[1,2,3],
+              [2,3,4], 
+              [3,4,5], 
+              [4,5,6], 
+              [5,6,7],
+              [6,7,8], 
+              [7,8,9]])
+y = np.array([4, 5, 6, 7, 8, 9, 10])
+
+print(x.shape, y.shape) # (7, 3), (7, )
+
+x = x.reshape(7, 3, 1)      # -> [[[1],[2],[3]], 
+                            #    [[2],[3],[4]], ...]
+print(x.shape)              # (7, 3, 1) 
+
+
+#2. 모델구성
+model = Sequential()
+# model.add(SimpleRNN(units = 10, input_shape=(3, 1)))
+#                     # (N , 3, 1) -> ([batch, timesteps, feature])
+model.add(LSTM(units = 10, input_shape=(3, 1)))
+model.add(Dense(5, activation='relu'))
+model.add(Dense(1))
+
+model.summary()
+
+# 심플
+# 10 * (10 + 1 + 1) = 120
+# units * ( feature + bias + units ) = parms
+
+# LSTM
+# 10 * (10 + 1 + 1) = 1
+# units * ( feature + bias + units ) = parms
