ff1e6cc9f72ad258dd367c07bdd68ee97542b057
MYeonP <pmyeon608@gmail.com>
2023-01-30 20:40:56 +0900

    0130_파이썬

---
+++ 자습/0130_int_float01.py
@@ -0,0 +1,14 @@
+output_a = int("52")
+output_b = float("52.273")
+
+print(type(output_a), output_a)
+print(type(output_b), output_b)
+
+
+"""
+<class 'int'> 52
+<class 'float'> 52.273
+-> 문자열이었던 "52"와 "52.273"이 각각 정수 52, 실수 52.273으로 변환
+
+"""
+
