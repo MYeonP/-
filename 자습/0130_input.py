ce1a5f47f4bb670b97fdc2f070330de01a759787
MYeonP <pmyeon608@gmail.com>
2023-01-30 20:40:45 +0900

    0130_파이썬

---
+++ 자습/0130_input.py
@@ -0,0 +1,20 @@
+# 입력을 받습니다
+string = input("입력> ")
+
+# 출력합니다
+print("자료: ", string)
+print("자료형: ", type(string))
+
+"""
+- 실행결과 1
+입력> 52273
+자료:  52273
+자료형:  <class 'str'>
+
+- 실행결과 2
+입력> True
+자료:  True
+자료형:  <class 'str'>
+
+
+"""
