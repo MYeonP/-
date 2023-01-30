185062ef4b4e449fed9a551d90658b36cc270620
MYeonP <pmyeon608@gmail.com>
2023-01-30 20:41:01 +0900

    0130_파이썬

---
+++ 자습/0130_int_float02.py
@@ -0,0 +1,19 @@
+input_a = float(input("첫 번째 숫자> "))
+input_b = float(input("두 번째 숫자> "))
+
+print("덧셈 결과: ", input_a + input_b)
+print("뺄셈 결과: ", input_a - input_b)
+print("곱셈 결과: ", input_a * input_b)
+print("나눗셈 결과: ", input_a / input_b)
+
+
+"""
+첫 번째 숫자> 273
+두 번째 숫자> 52
+덧셈 결과:  325.0
+뺄셈 결과:  221.0
+곱셈 결과:  14196.0
+나눗셈 결과:  5.25
+-> float 함수를 사용하여 소수점까지 출력
+
+"""
