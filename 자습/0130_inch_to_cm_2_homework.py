33c6a2d1738274d47a672067143863ad6c9d1385
MYeonP <pmyeon608@gmail.com>
2023-01-30 20:40:42 +0900

    0130_파이썬

---
+++ 자습/0130_inch_to_cm_2_homework.py
@@ -0,0 +1,26 @@
+str_input = input("숫자 입력> ")
+num_input = int(str_input)
+
+print()
+print(num_input, "inch")
+print((num_input*2.54), "cm")
+
+
+"""
+- 실행결과 1
+숫자입력> 1
+
+1 inch
+2.54 cm
+
+
+- 실행결과 2
+숫자 입력> 26
+
+26 inch
+66.04 cm
+
+-> SUCCESS!!!
+
+"""
+
