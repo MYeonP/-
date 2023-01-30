77ca9a2e911b1c32d3cf9a27870ee2028b0e3d67
MYeonP <pmyeon608@gmail.com>
2023-01-30 20:41:03 +0900

    0130_파이썬

---
+++ 자습/0130_str.py
@@ -0,0 +1,12 @@
+output_a = str(52)
+output_b = str(52.273)
+
+print(type(output_a), output_a)
+print(type(output_b), output_b)
+
+
+"""
+<class 'str'> 52
+<class 'str'> 52.273
+-> 숫자들이 문자열로 변환 됨
+"""
