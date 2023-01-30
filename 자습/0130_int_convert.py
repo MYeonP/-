6f728b80fe6b584388839514f65e1676418a3514
MYeonP <pmyeon608@gmail.com>
2023-01-30 20:40:53 +0900

    0130_파이썬

---
+++ 자습/0130_int_convert.py
@@ -0,0 +1,21 @@
+string_a = input("입력 A> ")
+int_a = int(string_a)
+
+string_b = input("입력 B> ")
+int_b = int(string_b)
+
+print("문자열 자료 : ", string_a + string_b)
+print("숫자 자료 : ", int_a + int_b)
+
+
+"""
+
+입력 A> 273
+입력 B> 52
+문자열 자료 :  27352
+숫자 자료 :  325
+
+- 문자열(string) 자료는 "273" + "52"를 문자로 연결하여 "27352"로 표시
+- 정수형(int) 자료는 273 + 52를 숫자로 인식하여 더한 325를 표시
+
+"""
