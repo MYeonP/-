b112f43df2a21f9d147c8194e3946a7851f4f966
MYeonP <pmyeon608@gmail.com>
2023-01-31 20:03:26 +0900

    0131_파이썬 자습

---
+++ 자습/0131_1_format_basic.py
@@ -0,0 +1,16 @@
+# format() 함수로 숫자를 문자열로 변환하기
+string_a = "{}".format(10)
+
+# 출력하기
+print(string_a)
+print(type(string_a))
+
+
+"""
+
+실행 결과
+
+print(string_a) : 10
+print(type(string_a)) : <class 'str'> (str = 문자열)
+
+"""
