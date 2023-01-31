d849e0ba292ca34cd95828fab0cf33937f47282e
MYeonP <pmyeon608@gmail.com>
2023-01-31 20:03:44 +0900

    0131_파이썬 자습

---
+++ 자습/0131_9_upper_lower.py
@@ -0,0 +1,19 @@
+# 대소문자 바꾸기 : upper()과 lower()
+
+a = "Hello Python Programming...!"
+
+print("upper() : 모두 대문자로 변환하기")
+print(a.upper())
+
+print("lower() : 모두 소문자로 변환하기")
+print(a.lower())
+
+
+"""
+
+upper() : 모두 대문자로 변환하기
+HELLO PYTHON PROGRAMMING...!
+lower() : 모두 소문자로 변환하기
+hello python programming...!
+
+"""
