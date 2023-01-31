6dcb2541dd7479fad9b10327a14af9ba4dbd1aa8
MYeonP <pmyeon608@gmail.com>
2023-01-31 20:03:40 +0900

    0131_파이썬 자습

---
+++ 자습/0131_7_format06.py
@@ -0,0 +1,16 @@
+output_a = "{:15.3f}".format(52.273)
+output_b = "{:15.2f}".format(52.273)
+output_c = "{:15.1f}".format(52.273)
+
+print(output_a)
+print(output_b)
+print(output_c)
+
+"""
+실행 결과
+         52.273
+          52.27
+           52.3
+-> 15칸을 잡고 소수점을 각각 3자리, 2자리, 1자리로 출력 / 자동으로 반올림 진행 
+
+"""
